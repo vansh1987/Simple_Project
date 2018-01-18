8b9a8eb3efa2908798ed67ce44b975c73d9627a5
Vansh <vanshstyle1987@gmail.com>
2018-01-18 14:01:52 +0530

    Update MainClass.java

--- src/com/MainClass.java
+++ src/com/MainClass.java
@@ -7,6 +7,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		//Console c = new Console();
 		//c.showMenu();
+		//System.out.println("Test");
 		Connection d = new Connection();
 		d.connectionSql();
 		d.createTable();
